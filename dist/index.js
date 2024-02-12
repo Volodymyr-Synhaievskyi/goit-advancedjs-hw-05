46a23bbcb7ac5862871d52609ed4f1f537c2a8cd
Volodymyr-Synhaievskyi <vovagidro@gmail.com>
2024-02-12 19:10:04 +0100

    453

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello world');
+let test = 1;
+let test2 = ' test value';
+console.log(test);
+console.log(test2);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello world');
+
+let test = 1;
+let test2 = ' test value';
+console.log(test);
+console.log(test2);
